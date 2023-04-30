8de9937b8b7e1ecac74f99435b95427990cb8331
Modestas Rimeikis <modestas.rimeikis@telesoftas.com>
2023-04-30 20:11:35 +0300

    Added example of POP usage in viewController

---
+++ SecondViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Presentation
+//
+//  Created by Modestas Rimeikis on 2023-03-29.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet private weak var mainButton: UIButton!
+    @IBOutlet private weak var secondaryButton: UIButton!
+    @IBOutlet private weak var thirdButton: UIButton!
+
+    @IBOutlet private weak var titleLabel: UITextField!
+
+//    var tracker: AppTrackable = AppTracker.shared
+
+    // MARK: Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    // MARK: Helpers
+    private func setupView() {
+        setupButtons()
+        setupTitleLabel()
+    }
+
+    // MARK: Actions
+    @objc func onFirstButtonTap(sender: UIButton!) {
+//        tracker.trackFirstButtonTap()
+        print("-----------------------------------------------")
+    }
+
+    @objc func onSecondButtonTap(sender: UIButton!) {
+//        tracker.trackSecondButtonTap()
+        print("-----------------------------------------------")
+    }
+
+    @objc func onThirdButtonTap(sender: UIButton!) {
+//        tracker.trackThirdButtonTap()
+        print("-----------------------------------------------")
+    }
+}
+
+private extension SecondViewController {
+    func setupButtons() {
+        mainButton.setTitle("Main button", for: .normal)
+        mainButton.addTarget(self, action: #selector(onFirstButtonTap),
+                             for: .touchUpInside)
+
+        secondaryButton.setTitle("Secondary button", for: .normal)
+        secondaryButton.addTarget(self, action: #selector(onSecondButtonTap),
+                                  for: .touchUpInside)
+
+        thirdButton.setTitle("Third button", for: .normal)
+        thirdButton.addTarget(self, action: #selector(onThirdButtonTap),
+                              for: .touchUpInside)
+    }
+
+
+    func setupTitleLabel() {
+        titleLabel.text = "Protocol-oriented development"
+    }
+}
